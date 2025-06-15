d5f881193e818e1f8eaf1c19cb1403962da45922
franccium <bawercx@gmail.com>
2025-06-15 20:41:26 +0200

    training refactor

--- utils/parsers.py
+++ utils/parsers.py
@@ -3,29 +3,6 @@ import os
 import re
 import pandas as pd
 
-def parse_sara(filename='sara.csv'):
-    file = os.path.join("..", "data", filename)
-    df = pd.read_csv(file)
-
-    def parse_composition(comp):
-        match = re.match(r"(\d+)(?=%[A-Z])(?!\d)%([A-Z]+)/(\d+)(?=%[A-Z])(?!\d)%([A-Z]+)", comp)
-        if not match:
-            raise ValueError("Input csv wrongly defined")
-        pct1, id1, pct2, id2 = match.groups()
-        return [id1, int(pct1), id2, int(pct2)]
-
-    parsed = df['Composition'].apply(parse_composition)
-    parsed_df = pd.DataFrame(parsed.tolist(), columns=[
-        'ID_1', '%_1', 'ID_2', '%_2'
-    ])
-
-    final_df = pd.concat([
-        df[['Nr']],
-        parsed_df,
-        df.drop(columns=['Nr', 'Composition'])
-    ], axis=1)
-    return final_df
-
 def parse_s_value(filename='s_value_As_types_s.csv'):
     file = os.path.join("data", filename)
     df = pd.read_csv(file)
@@ -41,9 +18,20 @@ def parse_s_value(filename='s_value_As_types_s.csv'):
     df['As2_scaled'] = df['As2'] * (df['%_2'])
     df['S1_scaled'] = df['S1'] * (df['%_1'])
     df['S2_scaled'] = df['S2'] * (df['%_2'])
+    
+    df['S_Value_sum'] = df['S_Value_part1'] + df['S_Value_part2']
+    df['S_Value_mean'] = (df['S_Value_part1'] + df['S_Value_part2']) / 2
+    df['S_Value_min'] = df[['S_Value_part1', 'S_Value_part2']].min(axis=1)
+    df['S_Value_max'] = df[['S_Value_part1', 'S_Value_part2']].max(axis=1)
 
-    result_df = df[['S_Value_part1', 'S_Value_part2', 'S_Value_res', 'As1_scaled', 'As2_scaled']]
+    df['As_scaled_sum'] = df['As1_scaled'] + df['As2_scaled']
+    df['As_scaled_mean'] = (df['As1_scaled'] + df['As2_scaled']) / 2
+    df['As_scaled_min'] = df[['As1_scaled', 'As2_scaled']].min(axis=1)
+    df['As_scaled_max'] = df[['As1_scaled', 'As2_scaled']].max(axis=1)
 
+    #result_df = df[['S_Value_part1', 'S_Value_part2', 'S_Value_res', 'As1_scaled', 'As2_scaled']]
+    result_df = df[['S_Value_mean', 'As_scaled_mean', 'S_Value_sum', 'S_Value_res']]
+    
     return result_df
 
 def parse_tsi_value(filename='tsi_value_with_As_Density.csv'):
@@ -61,9 +49,25 @@ def parse_tsi_value(filename='tsi_value_with_As_Density.csv'):
     df['As2_scaled'] = df['As2'] * (df['%_2'])
     df['D1_scaled'] = df['D1'] * (df['%_1'])
     df['D2_scaled'] = df['D2'] * (df['%_2'])
-    
-    result_df = df[['TSI_Value_part1', 'TSI_Value_part2', 'TSI_Value_res', 'D1_scaled', 'D2_scaled', 'As1_scaled', 'As2_scaled']]
 
+    df['TSI_Value_sum'] = df['TSI_Value_part1'] + df['TSI_Value_part2']
+    df['TSI_Value_mean'] = (df['TSI_Value_part1'] + df['TSI_Value_part2']) / 2
+    df['TSI_Value_min'] = df[['TSI_Value_part1', 'TSI_Value_part2']].min(axis=1)
+    df['TSI_Value_max'] = df[['TSI_Value_part1', 'TSI_Value_part2']].max(axis=1)
+
+    df['As_scaled_sum'] = df['As1_scaled'] + df['As2_scaled']
+    df['As_scaled_mean'] = (df['As1_scaled'] + df['As2_scaled']) / 2
+    df['As_scaled_min'] = df[['As1_scaled', 'As2_scaled']].min(axis=1)
+    df['As_scaled_max'] = df[['As1_scaled', 'As2_scaled']].max(axis=1)
+
+    df['D_scaled_sum'] = df['D1_scaled'] + df['D2_scaled']
+    df['D_scaled_mean'] = (df['D1_scaled'] + df['D2_scaled']) / 2
+    df['D_scaled_min'] = df[['D1_scaled', 'D2_scaled']].min(axis=1)
+    df['D_scaled_max'] = df[['D1_scaled', 'D2_scaled']].max(axis=1)
+    
+    #result_df = df[['TSI_Value_part1', 'TSI_Value_part2', 'TSI_Value_res', 'As1_scaled', 'As2_scaled']]
+
+    result_df = df[['TSI_Value_mean', 'As_scaled_mean', 'D_scaled_mean','TSI_Value_res', 'TSI_Value_sum']]
     return result_df
 
 def parse_p_value(filename='p_value_with_As_Density.csv'):
@@ -82,77 +86,25 @@ def parse_p_value(filename='p_value_with_As_Density.csv'):
     df['D1_scaled'] = df['D1'] * (df['%_1'])
     df['D2_scaled'] = df['D2'] * (df['%_2'])
     
-    result_df = df[['P_Value_part1', 'P_Value_part2', 'P_Value_res', 'As1_scaled', 'As2_scaled', 'D1_scaled', 'D2_scaled']]
+    #result_df = df[['P_Value_part1', 'P_Value_part2', 'P_Value_res', 'As1_scaled', 'As2_scaled', 'D1_scaled', 'D2_scaled']]
+    
+    df['P_Value_sum'] = df['P_Value_part1'] + df['P_Value_part2']
+    df['P_Value_mean'] = (df['P_Value_part1'] + df['P_Value_part2']) / 2
+    df['P_Value_min'] = df[['P_Value_part1', 'P_Value_part2']].min(axis=1)
+    df['P_Value_max'] = df[['P_Value_part1', 'P_Value_part2']].max(axis=1)
 
-    return result_df
+    df['As_scaled_sum'] = df['As1_scaled'] + df['As2_scaled']
+    df['As_scaled_mean'] = (df['As1_scaled'] + df['As2_scaled']) / 2
+    df['As_scaled_min'] = df[['As1_scaled', 'As2_scaled']].min(axis=1)
+    df['As_scaled_max'] = df[['As1_scaled', 'As2_scaled']].max(axis=1)
 
-def logit(p, eps=1e-6):
-    import numpy as np
-    p = np.clip(p, eps, 1 - eps)
-    return np.log(p / (1 - p))
+    df['D_scaled_sum'] = df['D1_scaled'] + df['D2_scaled']
+    df['D_scaled_mean'] = (df['D1_scaled'] + df['D2_scaled']) / 2
+    df['D_scaled_min'] = df[['D1_scaled', 'D2_scaled']].min(axis=1)
+    df['D_scaled_max'] = df[['D1_scaled', 'D2_scaled']].max(axis=1)
 
-def parse_asmix(filename='mieszaniny.csv'):
-    file = os.path.join("data", filename)
-    df = pd.read_csv(file)
-    
-    from utils.augmentation import augment_data
-    columns_to_augment = [
-        'S1', 'Ar1', 'R1', 'As1', '%1',
-        'S2', 'Ar2', 'R2', 'As2', '%2', 'AsMix'
-    ]
-    dfa = augment_data(df, columns_to_augment, 3, 15, 0.05)
-    df = pd.concat([dfa, df])
-    
-    df['S1_scaled'] = df['S1'] * (df['%1'] / 100)
-    df['Ar1_scaled'] = df['Ar1'] * (df['%1'] / 100)
-    df['R1_scaled'] = df['R1'] * (df['%1'] / 100)
-    df['As1_scaled'] = df['As1'] * (df['%1'] / 100)
-    df['S2_scaled'] = df['S2'] * (df['%2'] / 100)
-    df['Ar2_scaled'] = df['Ar2'] * (df['%2'] / 100)
-    df['R2_scaled'] = df['R2'] * (df['%2'] / 100)
-    df['As2_scaled'] = df['As2'] * (df['%2'] / 100)
-    
-    #todo typ ropy
-    features = [
-        'As1_scaled', '%1', 'S1_scaled', 'R1_scaled', 'Ar1_scaled',
-        'As2_scaled', '%2', 'S2_scaled', 'R2_scaled', 'Ar2_scaled'
-    ]
-    target = 'AsMix'
-    result_df = df[features + [target]]
-    return result_df
+    result_df = df[['P_Value_mean', 'P_Value_sum','As_scaled_sum', 'As_scaled_mean','D_scaled_sum', 'D_scaled_mean', 'P_Value_res']]
 
-
-def parse_asmix_with_density(filename='mieszaniny_sara_with_density.csv'):
-    file = os.path.join("data", filename)
-    df = pd.read_csv(file)
-    target = 'AsMix'
-    
-    from utils.augmentation import augment_data
-    columns_to_augment = [
-        'S1', 'Ar1', 'R1', 'As1', '%1', 'D1', 'D2',
-        'S2', 'Ar2', 'R2', 'As2', '%2', 'AsMix'
-    ]
-    dfa = augment_data(df, columns_to_augment, 3, 25, 0.05)
-    df = pd.concat([dfa, df])
-    
-    df['D1_scaled'] = df['D1'] * (df['%1'] / 100)
-    df['D2_scaled'] = df['D2'] * (df['%2'] / 100)
-    
-    df['S1_scaled'] = df['S1'] * (df['%1'] / 100)
-    df['Ar1_scaled'] = df['Ar1'] * (df['%1'] / 100)
-    df['R1_scaled'] = df['R1'] * (df['%1'] / 100)
-    df['As1_scaled'] = df['As1'] * (df['%1'] / 100)
-    df['S2_scaled'] = df['S2'] * (df['%2'] / 100)
-    df['Ar2_scaled'] = df['Ar2'] * (df['%2'] / 100)
-    df['R2_scaled'] = df['R2'] * (df['%2'] / 100)
-    df['As2_scaled'] = df['As2'] * (df['%2'] / 100)
-    
-    #todo typ ropy
-    features = [
-        'D1_scaled', 'As1_scaled',  'S1_scaled', 'R1_scaled', 'Ar1_scaled',
-        'D2_scaled', 'As2_scaled', 'S2_scaled', 'R2_scaled', 'Ar2_scaled'
-    ]
-    result_df = df[features + [target]]
     return result_df
 
 def parse_asmix_with_density_find_CII(filename='mieszaniny_sara_types_appended.csv'):
@@ -180,30 +132,9 @@ def parse_asmix_with_density_find_CII(filename='mieszaniny_sara_types_appended.c
     df['R2_scaled'] = df['R2'] * (df['%2'])
     df['As2_scaled'] = df['As2'] * (df['%2'])
     
-    #todo typ ropy
     features = [
         'D1_scaled', 'As1_scaled',  'S1_scaled', 'R1_scaled', 'Ar1_scaled',
         'D2_scaled', 'As2_scaled', 'S2_scaled', 'R2_scaled', 'Ar2_scaled',
     ]
     result_df = df[features + [target]]
     return result_df
-
-def generate_types():
-    import joblib
-
-    clf = joblib.load('models\\random forest_density_group.pkl')
-
-    df = pd.read_csv('data\\mieszaniny_sara_with_density copy.csv')
-
-    features = ['D', 'S', 'Ar', 'R', 'As']
-
-    oil1 = df[['D1', 'S1', 'Ar1', 'R1', 'As1']].copy()
-    oil1.columns = ['Density', 'S', 'Ar', 'R', 'As']
-
-    oil2 = df[['D2', 'S2', 'Ar2', 'R2', 'As2']].copy()
-    oil2.columns = ['Density', 'S', 'Ar', 'R', 'As']
-
-    df['Type1'] = clf.predict(oil1)
-    df['Type2'] = clf.predict(oil2)
-
-    df.to_csv('classified_output.csv', index=False)
